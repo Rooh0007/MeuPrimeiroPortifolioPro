c5aa610e1da7acb37133e0e045b455022709b422
Rodolpho <Rodolpho.010315@gmail.com>
2022-09-01 13:38:27 -0300

    portifolio

---
+++ arq.js
@@ -0,0 +1,7 @@
+var animado = document.getElementsByClassName('ring')
+//animado.style.transform = `translate(${Math.random(10)},${Math.random(10)})`
+console.log('rodolpho')
+console.log(animado)
+animado.map(()=>{
+    
+})
